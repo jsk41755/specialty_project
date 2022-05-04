94631b392250564e30eabbf8f70d8636915f503c
jsk41755 <43807509+jsk41755@users.noreply.github.com>
2022-05-04 14:06:27 +0900

    Add files via upload

---
+++ 9week_10_1/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.a9week_10_1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+
+        Button btnReturn = (Button) findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
